0c67b3aacef018dbf04abe335d53b308199e0e5d
Marsky <you@example.com>
2025-06-25 22:48:51 +0300

    add __init__.py for centralized model exports

---
+++ app/models/__init__.py
@@ -0,0 +1,15 @@
+from app.models.cart_items import CartItemOrm
+from app.models.categories import CategoryOrm
+from app.models.comments import CommentOrm
+from app.models.orders import OrderOrm
+from app.models.products import ProductOrm
+from app.models.users import UserOrm
+
+__all__ = [
+    'CartItemOrm',
+    'CategoryOrm',
+    'CommentOrm',
+    'OrderOrm',
+    'ProductOrm',
+    'UserOrm'
+]
